17c4332be2c4b304b0113a1e6478089701814de8
sim1pas <simon.pascal.ngos@fnacdarty.com>
2019-12-16 18:12:37 +0100

    Init project

---
+++ src/main/java/org/simon/pascal/TransportAppApplication.java
@@ -0,0 +1,13 @@
+package org.simon.pascal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportAppApplication.class, args);
+	}
+
+}
